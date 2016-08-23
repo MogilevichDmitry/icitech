d503266386fcecf3ab2595781f29948584f84ce7
MogilevichDmitry <dmsmain@gmail.com>
2016-08-23 11:43:29 +0300

    Add papersheet close-btn animation

--- js/common.js
+++ js/common.js
@@ -7,12 +7,8 @@ $(document).ready(function () {
 
         if (_papersheet.hasClass("opened")) {
             $(this).parent(".papersheet").stop().removeClass("opened");
-            $(".feedback-btn").fadeIn(400);
-            $(".papersheet__close-icon").fadeOut(50);
         } else {
             $(this).parent(".papersheet").stop().addClass("opened");
-            $(".feedback-btn").fadeOut(50);
-            $(".papersheet__close-icon").fadeIn(400);
         }
     });
 });
